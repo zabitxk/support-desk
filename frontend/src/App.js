453da393230c297f8897d63ca35ae0541cf47ea5
zabitxk <94786825+zabitxk@users.noreply.github.com>
2022-03-30 20:21:29 +0700

    PrivateRoute for NewTicket page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,10 @@ import "react-toastify/dist/ReactToastify.css"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
+import NewTicket from "./pages/NewTicket"
 import Header from "./components/Header"
+import PrivateRoute from "./components/PrivateRoute"
+
 function App() {
   return (
     <>
@@ -15,6 +18,9 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/new-ticket" element={<PrivateRoute />}>
+              <Route path="/new-ticket" element={<NewTicket />} />
+            </Route>
           </Routes>
         </div>
       </Router>
---
+++ frontend/src/pages/NewTicket.jsx
@@ -0,0 +1,9 @@
+function NewTicket() {
+    return (
+        <div>
+            <h1>new ticket</h1>
+        </div>
+    )
+}
+
+export default NewTicket
