4f7564e176f738e81f38bf5f1f10f65798d0963b
zabitxk <94786825+zabitxk@users.noreply.github.com>
2022-03-30 13:12:03 +0700

    Logout function init

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -37,6 +37,12 @@ export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
   console.log(user)
 })
 
+// Logout user
+
+export const logout = createAsyncThunk("auth/logout", async () => {
+  await authService.logout
+})
+
 export const authSlice = createSlice({
   name: "auth",
   initialState,
@@ -62,6 +68,9 @@ export const authSlice = createSlice({
         state.isLoading = false
         state.isError = true
         state.message = action.payload
+        state.user = null
+      })
+      .addCase(logout.fulfilled, (state) => {
         state.user = null
       })
   },
